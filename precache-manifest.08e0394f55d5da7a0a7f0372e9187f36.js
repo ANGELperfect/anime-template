d88a5d3197095adc2860a3ffa0f8d1f8318c532e
Your Name <you@example.com>
2020-09-19 20:42:59 +0100

    Updates

---
+++ precache-manifest.08e0394f55d5da7a0a7f0372e9187f36.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a797c5a0714d64b2b7b8",
+    "url": "/anime-template/static/css/main.781501de.chunk.css"
+  },
+  {
+    "revision": "a797c5a0714d64b2b7b8",
+    "url": "/anime-template/static/js/main.ead8715a.chunk.js"
+  },
+  {
+    "revision": "ccd548983677303635f8",
+    "url": "/anime-template/static/js/runtime~main.1a50e4cf.js"
+  },
+  {
+    "revision": "0e92877164d3cfa52d9f",
+    "url": "/anime-template/static/js/2.af333514.chunk.js"
+  },
+  {
+    "revision": "45dd54703cf1537d439bc358c680762c",
+    "url": "/anime-template/static/media/star.45dd5470.png"
+  },
+  {
+    "revision": "80c60f92cfb4cc60b8c2ecd67c339f58",
+    "url": "/anime-template/static/media/twitter (1).80c60f92.png"
+  },
+  {
+    "revision": "bcc3e469f0b23903a55e62f057fcc19e",
+    "url": "/anime-template/static/media/bg.bcc3e469.jpg"
+  },
+  {
+    "revision": "2aa72ec0b8a81cd78e9c994d9d13a8d5",
+    "url": "/anime-template/static/media/cover.2aa72ec0.jpg"
+  },
+  {
+    "revision": "1045374d5b976d6a5a811d84349108e9",
+    "url": "/anime-template/static/media/rating.1045374d.png"
+  },
+  {
+    "revision": "1a6f4faac47975ab6a8bcd5baf951520",
+    "url": "/anime-template/index.html"
+  }
+];
