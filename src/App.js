7523c3fff3f0069c58e3a2c26822c57dfb5f5c74
Your Name <you@example.com>
2020-09-19 20:41:33 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AnimeBg from './components/anime-bg'
+import AnimeCover from './components/anime-cover'
+import star from './star.png'
+import fb from './facebook (1).png'
+import tw from './twitter (1).png'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <AnimeBg/>
+      <AnimeCover/>
+      <div className="status"><h3>Finished</h3></div>
+      <div className="watch">
+        <div><h2>Watch</h2></div>
+        <img src={star} alt="rate"/>
+      </div>
+      <div className="card-infos">
+        <h1>Naruto Shippuden</h1>
+        
+        <div className="cat"><p>Action</p></div>
+        <div className="rate">
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div className="infos"><p><span>Author : </span>Masashi Kishimoto</p></div>
+        <div className="infos"><p><span>Episodes : </span>500</p></div>
+        <div className="infos"><p><span>Subtitles : </span>English</p></div>
+      </div>
+      <div className="desc">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span>Description : </span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris 
+          vitae ultricies leo integer malesuada nunc vel risus .. 
+          <span> show more </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="footer">
+        <p>Folow Us</p>
+        <img src={tw} alt="twitter"/>
+        <img src={fb} alt="facebook"/>
+      </div>
     </div>
   );
 }
---
+++ src/components/anime-cover.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const AnimeCover = () =>{
+    return(
+        <div className="anime-cover">
+        </div>
+    )
+}
+
+
+
+export default AnimeCover
